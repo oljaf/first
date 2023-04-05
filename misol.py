e403095e2c6247e1bffedf50ba34acfa74a5eb05
MOONBEKxoja <gishtmurodtoshmurodov@gmail.com>
2023-04-05 19:39:34 +0500

    Ruslan

---
+++ misol.py
@@ -0,0 +1,74 @@
+# ismlar = []
+# ismlar.append("Temur")
+# ismlar.append("Davlat")
+# ismlar.append("Jonibek")
+# print(ismlar)                
+
+# def ism(a):
+#     names = []
+#     for k in range(a):
+#         b = input("Ism kiriting:")
+#         ism.append(b)
+# ism(int(input("Nechta ism kiritmoqchisiz?:")))
+
+# def son(a):
+#     Sonlar = []
+#     for k in range(a):
+#         b = float(input())
+#         Sonlar.append(b)
+#     print(Sonlar)
+# son(int(input("Qancha sonlar kiritmoqchisiz?:")))
+
+# def son(a):
+#     t_shaxslarva = []
+#     t_shaxslar = []
+#     for k in range(a):
+#         c = input()
+#         t_shaxslarva.append(c)
+#     print(t_shaxslarva)~
+#     b = int(input("Zamonamiz Vatanparvarlardan qanchasin bilasiz?:"))
+#     for k in range(b):
+#         d = input()
+#         t_shaxslar.append(d)
+#     print(t_shaxslar)
+# son(int(input("Vafot etgan qancha Vatanparvarlarni bilasiz?:")))
+
+# sonlar = []
+# for k in range(120,1200+1):
+#     if k%2==0:
+#         sonlar.append(k)
+#     print(sonlar)
+
+# def son(a):
+#     s = 0
+#     sonlar = []
+#     for k in range(a):
+#         s = s + k
+#         sonlar.append(s)
+#         print(sonlar)
+# son(int(input()))
+
+# def son(a):
+#     sonlar = []
+#     son1 = sonlar[0]
+#     son2 = sonlar[0]
+#     for k in range(a):
+#         if k>sonlar:
+#             b = int(input())
+#         sonlar.append(b)
+#         print(sonlar)
+# son(int(input("Qancha son kiritmoqchisiz?:")))
+
+# son1 = [1]
+# son2 = [0]
+# son1.extend(son2)
+# print(son1)
+
+# def ovqat(a):
+#     taom = []
+#     for k in range(a):
+#         b = input()
+#         taom.append(b)
+#         print(taom)
+# ovqat(int(input("")))
+
